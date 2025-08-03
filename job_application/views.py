718f460ecea61bcc366a3dc5bff0721ca0832c9a
“rameel <rameelahmed786@gmail.com>
2025-08-04 01:29:23 +0500

    initial commit

---
+++ job_application/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+
+def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            
+            Form.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                date=date,
+                occupation=occupation
+            )
+
+            messages.success(request, "Form Submitted Successfully.")
+            return redirect("index")
+    else:
+        form = ApplicationForm()
+
+    return render(request, "index.html", {"form": form})
+
+
+def about(request):
+        return render(request, "about.html")
+
+    
